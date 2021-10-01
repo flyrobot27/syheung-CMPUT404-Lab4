9a8662bf6732b1bbc0fad87ef96e2c4c68ea193c
flyrobot27 <stevenheung.ca@gmail.com>
2021-09-30 22:54:11 -0600

    Question 8 complete

--- polls/urls.py
+++ polls/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # /polls/(question_id: int)/
-    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
     # /polls/(question_id: int)/results/
-    path('<int:question_id>/results/', views.results, name="results"),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name="results"),
     # /polls/(question_id: int)/vote/
     path('<int:question_id>/vote/', views.vote, name="vote")
 ]
